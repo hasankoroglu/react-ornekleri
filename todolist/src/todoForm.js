f5ecf6554cd7d989d43c3b8da0cf4694848d2988
hasankoroglu <hasan@hasankoroglu.com>
2019-06-11 16:55:14 +0300

    todo

--- todolist/src/todoForm.js
+++ todolist/src/todoForm.js
@@ -13,11 +13,7 @@ class ToDoForm extends React.Component{
             <div>
                 <div className="todo type1">
                     <form className="input-wrapper" onSubmit={this.addTask}>
-<<<<<<< HEAD
-                        <input id="todoInput" type="text" className="add-todo" />
-=======
                         <input id="todoInput" type="text" className="add-todo" autoComplete="off" />
->>>>>>> 8e47e721cb5c40b2bd4359060f929993dfe3d8f3
                     </form>
                 </div>
                 <button type="button" className="add-btn" onClick={this.addTask}></button>
