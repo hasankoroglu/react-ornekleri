5774cf6fc4e06f68cb73dabb60d6281cbcd2d0b2
hasankoroglu <hasan@hasankoroglu.com>
2019-06-25 14:00:19 +0300

    gems style

---
+++ mangala/src/js/Pit.js
@@ -0,0 +1,24 @@
+import React from "react";
+import gem from "../img/gem.png"
+
+function Pit(){
+    return(
+        <div>
+            <span>
+                <div className="gemsPit">
+                <img className="gem" src={gem} alt="Gem" />
+                <img className="gem" src={gem} alt="Gem" />
+                <img className="gem" src={gem} alt="Gem" />
+                <img className="gem" src={gem} alt="Gem" />
+                <img className="gem" src={gem} alt="Gem" />
+                <img className="gem" src={gem} alt="Gem" />
+                <img className="gem" src={gem} alt="Gem" />
+                <img className="gem" src={gem} alt="Gem" />
+                <img className="gem" src={gem} alt="Gem" />
+                </div>
+            </span>
+        </div>
+    );
+}
+
+export default Pit;
---
+++ mangala/src/js/Store.js
@@ -0,0 +1,51 @@
+import React from "react";
+import gem from "../img/gem.png"
+
+function Store() {
+    return (
+        <div>
+            <div className="store">
+                <div className="gemsStore">
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                    <img className="gem" src={gem} alt="Gem" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Store;
