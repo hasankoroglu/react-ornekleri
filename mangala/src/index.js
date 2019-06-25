221d753c11d6366bc994b7b6dcca22750306d316
hasankoroglu <hasan@hasankoroglu.com>
2019-06-25 10:40:29 +0300

    cleanup

--- mangala/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import FormContainer from "./FormContainer";
-
-const wrapper = document.getElementById("create-article-form");
-wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
