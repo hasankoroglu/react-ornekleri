433a6cab9589d7af9d970fa93bcb6f8aabde3b29
hasankoroglu <hasan@hasankoroglu.com>
2019-07-07 16:48:51 +0300

    hede

--- hasanhooks/src/js/Board.js
+++ hasanhooks/src/js/Board.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Store from "./Store";
 import Pit from "./Pit";
 
--- hasanhooks/src/js/Pit.js
+++ hasanhooks/src/js/Pit.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Gem from "./Gem";
 
 export default function Pit({ index, id, gems, onPitClick }) {
