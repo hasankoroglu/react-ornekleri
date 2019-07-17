df56d0536b63c84c34d3f0a73723553a1739f52f
hasankoroglu <hasan@hasankoroglu.com>
2019-07-17 18:27:34 +0300

    playersTurn

--- mangala/src/js/App.js
+++ mangala/src/js/App.js
@@ -1,26 +1,55 @@
 import React, { useState } from "react";
 import Board from "./Board";
 import { Button, Modal, Form } from "react-bootstrap";
+import arrowPng from "../img/arrow.png";
+
+const PlayerName = props => {
+  return (
+    <div className="headingDiv">
+      <span class="wrapper">
+      <img src={arrowPng} alt="" className={props.playersTurn.length>0?(props.playersTurn === props.playersName ? "show" : "hide"):"hide"} />
+      <span className="heading">
+        {props.playersName}
+      </span>
+      </span>
+    </div>
+  );
+};
 
 function App() {
-  const [pitsGems, setPitsGems] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const initialPlayers = { user1: "", user2: "" };
+  const [pitsGems, setPitsGems] = useState([0,0,0,0,0,0,0,0,0,0,0,0]);
   const [gameStatus, setGameStatus] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [players, setPlayers] = useState({user1:'',user2:''});
-  const [playersTurn, setPlayersTurn] = useState('');
+  const [players, setPlayers] = useState(initialPlayers);
+  const [playersTurn, setPlayersTurn] = useState("");
+  const [validated, setValidated] = useState(false);
 
   const handleModalClose = () => setShowModal(false);
   const handleModalShow = () => setShowModal(true);
 
-  const startGame = () => {
-    changePitGems(4);
-    setGameStatus(true);
-    setPlayersTurn(`${players.user1}`);
-    handleModalClose();
+  const startGame = event => {
+    const form = event.currentTarget;
+
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      setValidated(true);
+      changePitGems(4);
+      setGameStatus(true);
+      setPlayersTurn(players.user1);
+      handleModalClose();
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
   };
 
   const resetGame = () => {
     changePitGems(0);
+    setPlayers(initialPlayers);
+    setPlayersTurn("");
     setGameStatus(false);
   };
 
@@ -33,31 +62,38 @@ function App() {
     );
   };
 
-	const handleInputChange = event => {
-    const { id, value } = event.target
-		setPlayers({...players, [id]: value })
-	}
+  const handleInputChange = event => {
+    const { id, value } = event.target;
+    setPlayers({ ...players, [id]: value });
+  };
 
   const onPitClick = param => {
     setPitsGems(pitsGems => {
       let tmpPitsGems = [...pitsGems];
       let nextPits = tmpPitsGems[param] - 1;
-      tmpPitsGems[param]=1;
+      tmpPitsGems[param] = 1;
       let i = 1;
       for (i; i <= nextPits; i++) {
         tmpPitsGems[param + i]++;
       }
       return tmpPitsGems;
     });
-    playersTurn===players.user1 ? setPlayersTurn(`${players.user2}`) : setPlayersTurn(`${players.user1}`);
+
+    setPlayersTurn(
+      playersTurn === players.user1 ? players.user2 : players.user1
+    );
   };
 
   return (
     <div>
-      <h1>{players.user2}</h1>
+      <PlayerName playersTurn={playersTurn} playersName={players.user2} />
       <Board pitsGems={pitsGems} onPitClick={index => onPitClick(index)} />
-      <h1>{players.user1}</h1>
-      <Button className="startReset" type="button" onClick={gameStatus ? resetGame : handleModalShow}>
+      <PlayerName playersTurn={playersTurn} playersName={players.user1} />
+      <Button
+        className="startReset"
+        type="button"
+        onClick={gameStatus ? resetGame : handleModalShow}
+      >
         {gameStatus ? "Baştan Başla" : "Başla"}
       </Button>
       <Modal show={showModal} onHide={handleModalClose}>
@@ -68,12 +104,27 @@ function App() {
           Lütfen oyuncu isimlerini giriniz.
           <br />
           <br />
-          <Form>
+          <Form
+            noValidate
+            validated={validated}
+            id="playersNames"
+            onSubmit={startGame}
+          >
             <Form.Group controlId="user1">
-              <Form.Control type="text" placeholder="1. Oyuncu" onChange={handleInputChange} />
+              <Form.Control
+                type="text"
+                placeholder="1. Oyuncu"
+                onChange={handleInputChange}
+                required
+              />
             </Form.Group>
             <Form.Group controlId="user2">
-              <Form.Control type="text" placeholder="2. Oyuncu" onChange={handleInputChange} />
+              <Form.Control
+                type="text"
+                placeholder="2. Oyuncu"
+                onChange={handleInputChange}
+                required
+              />
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -81,7 +132,7 @@ function App() {
           <Button variant="secondary" onClick={handleModalClose}>
             İptal
           </Button>
-          <Button variant="primary" onClick={startGame}>
+          <Button variant="primary" type="submit" form="playersNames">
             Başla
           </Button>
         </Modal.Footer>
