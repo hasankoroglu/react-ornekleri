a1274a46ce4652c60a7a1f07f64a1c33fb5e7301
hasankoroglu <hasan@hasankoroglu.com>
2019-08-01 17:55:30 +0300

    cleanup

--- README.md
+++
@@ -1,3 +0,0 @@
-# React Örnekleri
-
-React öğrenirken kullandığım veya incelediğim örnekleri buradan paylaşmaya çalışacağım.
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./js/App";
-import "./css/index.css";
-import "./css/bootstrap.min.css";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
