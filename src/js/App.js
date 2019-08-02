47a9fd4ec5ecf3ad18c5905af600978669da3f07
hasankoroglu <hasan@hasankoroglu.com>
2019-08-02 23:04:50 +0300

    context

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import ContextExample from "./context"
 
 function App() {
-
   return (
-    <div>
-      Hello React World!
-    </div>
+    <Router>
+        <Header/>
+        <Route path="/" exact component={Home} />
+        <Route path="/context/" component={ContextExample} />
+    </Router>
   );
+}
+
+const Home = () => {
+  return(<h2>Anasayfa</h2>);
+}
+
+const Header = () => {
+  return(<nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <NavLink exact to="/" className="nav-link" activeClassName="active">Anasayfa</NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink exact to="/context/" className="nav-link" activeClassName="active">Context</NavLink>
+      </li>
+    </ul>
+  </nav>);
 }
 
 export default App;
