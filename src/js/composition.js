86f04e1504e1b7ac6e5225b57d71ee8cb4a91c6d
hasankoroglu <hasan@hasankoroglu.com>
2019-08-09 17:20:08 +0300

    pipe

--- src/js/composition.js
+++ src/js/composition.js
@@ -1,13 +1,41 @@
 import React from "react";
 
 const Composition = () => {
-    const _pipe = (f, g) => (...args) => g(f(...args))
+    // const _pipe = (f, g) => (...args) => g(f(...args))
 
-    const pipe = (...fns) => fns.reduce(_pipe);
+    function _pipe(f,g) {
+        console.log("_pipe f",f);
+        console.log("_pipe g",g);
+        return (
+           function hede(...args) {
+            console.log("_pipe ...args",...args);
+               return g(f(...args))
+           }
+        )
+    }
+
+    // const pipe = (...fns) => fns.reduce(_pipe);
+
+    function pipe(...fns){
+        // console.log("pipe fns",...fns);
+        return fns.reduce(_pipe);
+    }
     
-    const partial = (fn, ...args) => fn.bind(null, ...args);
+    // const partial = (fn, ...args) => fn.bind(null, ...args);
     
-    const add = (a, b, c = 0) => a + b + c;
+    function partial(fn, ...args){
+        // console.log("partial args",...args);
+        return fn.bind(null,...args)
+    }
+
+    // const add = (a, b, c = 0) => a + b + c;
+
+    function add(a, b, c = 0) {
+        // console.log("a",a);
+        // console.log("b",b);
+        // console.log("c",c);
+        return a + b + c;
+    }
     
     const multiply = (a, b) => a * b;
     
@@ -17,7 +45,8 @@ const Composition = () => {
     
     const result = comeTogether(2, 4);
 
-    console.log(result);
+    console.log("result",result);
+    // console.log("result",partial(add, 10, 3)());
     
     return <div />;
 }
